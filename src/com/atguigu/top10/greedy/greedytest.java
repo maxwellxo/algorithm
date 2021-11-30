fd3fa46ad0a8d8de525e419c7896c0a84260f2a9
Jacky <jackyxue@bupt.edu.cn>
2021-11-30 20:20:52 +0800

    second commit

--- src/com/atguigu/top10/greedy/greedytest.java
+++ src/com/atguigu/top10/greedy/greedytest.java
@@ -3,5 +3,6 @@ package com.atguigu.top10.greedy;
 public class greedytest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git222");
     }
 }
